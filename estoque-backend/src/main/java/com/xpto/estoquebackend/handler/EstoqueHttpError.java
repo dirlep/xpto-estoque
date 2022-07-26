ef1f13fd4ade246057a8832af8911aace79507a8
Dirlene Preilipper <dirlene.preilipper@gmail.com>
2022-07-26 10:37:42 -0300

    chore: ajustes handler

--- estoque-backend/src/main/java/com/xpto/estoquebackend/handler/EstoqueHttpError.java
+++ estoque-backend/src/main/java/com/xpto/estoquebackend/handler/EstoqueHttpError.java
@@ -2,6 +2,13 @@ package com.xpto.estoquebackend.handler;
 
 import org.springframework.http.HttpStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class EstoqueHttpError {
 
 	private String message;
@@ -11,52 +18,11 @@ public class EstoqueHttpError {
 	private String status;
 
 	private String timestamp;
-	
-	public EstoqueHttpError() {}
-
-	public EstoqueHttpError(String message, String detail, String status, String timestamp) {
-		this.message = message;
-		this.detail = detail;
-		this.status = status;
-		this.timestamp = timestamp;
-	}
 
 	public EstoqueHttpError(String message, String detail, HttpStatus httpStatus, String timestamp) {
 		this.message = message;
 		this.detail = detail;
 		this.status = httpStatus.toString();
-		this.timestamp = timestamp;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getDetail() {
-		return detail;
-	}
-
-	public void setDetail(String detail) {
-		this.detail = detail;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
 	}
 
