6195af4314da5879556fc1afef7b9137e3462e22
Dirlene Preilipper <dirlene.preilipper@gmail.com>
2022-07-26 10:05:16 -0300

    feature: criando os projetos backend e frontend

---
+++ estoque-backend/src/test/java/com/xpto/estoquebackend/EstoqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpto.estoquebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
