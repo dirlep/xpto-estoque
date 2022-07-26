9e942940a4cfab7ca60a0b78c49cd5933df1be80
Dirlene Preilipper <dirlene.preilipper@gmail.com>
2022-07-26 10:38:48 -0300

    feature: adicionando import.sql

---
+++ estoque-backend/src/main/resources/import.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0001, 'Micro-ondas Electrolux MTD30 20 litros', 'Eletrodoméstico', 350.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0002, 'Lavadora de Roupas Electrolux LES11 11Kg', 'Eletrodoméstico', 1200.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0003, 'Geladeira Frost Free Electrolux TW42S 382L', 'Eletrodoméstico', 2800.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0004, 'Cooktop 5 Bocas Itatiaia', 'Eletrodoméstico', 268.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0005, 'Ar Condicionado Split Consul 12.000 Btus Frio', 'Eletrodoméstico', 1350.00, 0, 0);
+
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0006, 'Smartphone Samsung Galaxy A03 Core 32GB', 'Eletrônico', 525.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0007, 'Smartphone Motorola Moto G22 128GB', 'Eletrônico', 980.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0008, 'Smart TV Philips Android Tela 55', 'Eletrônico', 2200.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0009, 'Samsung Smart TV 58 UHD 4k', 'Eletrônico', 2720.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0010, 'Console Xbox Series S 512gb', 'Eletrônico', 1670.00, 0, 0);
+
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0011, 'Escrivaninha 1 Porta 1 Gaveta Vitória Demóbile', 'Móvel', 110.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0012, 'Cadeira de Escritorio com base cromada Prizi Essencial', 'Móvel', 235.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0013, 'Cadeira Gamer Azul Prizi ', 'Móvel', 540.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0014, 'Cadeira Diretor Ergonômica Qualiflex', 'Móvel', 420.00, 0, 0);
+INSERT INTO tb_produto(codigo, descricao, tipo_produto, valor_fornecedor, quantidade_estoque, quantidade_saida) VALUES (0015, 'Cadeira Gamer Ecoflex', 'Móvel', 465.00, 0, 0);
